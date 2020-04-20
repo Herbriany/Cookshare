18136d3e6ac18b29c762aa3d79a49991346afbfb
Brian Derrig <DERRIGB@argos.co.uk>
2020-04-20 15:32:00 +0100

    Added Profile Image upload logic

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,9 @@
 const Review = require('../models/review');
 const User = require('../models/user');
-const Post = require('../models/post')
+const Post = require('../models/post');
+const { cloudinary } = require ('../cloudinary');
 
-module.exports = {
+const middleware = {
     asyncErrorHandler: (fn) =>
         (req, res, next) => {
             Promise.resolve(fn(req, res, next))
@@ -38,6 +39,7 @@ module.exports = {
             next();
         }
         else {
+            middleware.deleteProfileImage(req);
             req.session.error = 'Incorrect Password!';
             return res.redirect('/profile');
         }
@@ -48,6 +50,7 @@ module.exports = {
             passwordConfirmation
         } = req.body;
         if (newPassword && !passwordConfirmation) {
+            middleware.deleteProfileImage(req);
             req.session.error = 'Missing password confirmation';
             return res.redirect('/profile');
         }
@@ -58,6 +61,7 @@ module.exports = {
                 next();
             }
             else {
+                middleware.deleteProfileImage(req);
                 req.session.error = 'New passwords must match';
                 return res.redirect('/profile');
             }
@@ -65,5 +69,10 @@ module.exports = {
         else {
             next();
         }
+    },
+    deleteProfileImage : async req => {
+        if (req.file) await cloudinary.v2.uploader.destroy(req.file.public_id);
     }
-}
+};
+
+module.exports = middleware;
